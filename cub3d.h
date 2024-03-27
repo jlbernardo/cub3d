d10440e173c09dd006c38a678dc5c3a04cb6a0ca
Juliany <julianybernardo@outlook.com>
2024-03-27 18:22:36 -0300

    (chore) Add allowed functions libraries to header file

---
+++ cub3d.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Juliany Bernardo <julberna@student.42sp    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/27 18:15:38 by Juliany Ber       #+#    #+#             */
+/*   Updated: 2024/03/27 18:22:17 by Juliany Ber      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "libft.h"
+# include <math.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <errno.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/stat.h>
+
+#endif
