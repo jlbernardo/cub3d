cd628550b562cb54d1d5d7f22e9fef3f0fe54085
Juliany <julianybernardo@outlook.com>
2024-04-12 13:46:43 -0300

    (feat #19) Minimap! Pretty good scaling and faithful positioning

--- src/minimap.c
+++ src/minimap.c
@@ -6,7 +6,7 @@
 /*   By: julberna <julberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 18:00:41 by julberna          #+#    #+#             */
-/*   Updated: 2024/04/11 18:39:54 by julberna         ###   ########.fr       */
+/*   Updated: 2024/04/12 13:43:05 by julberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,18 @@ void	draw_minimap(t_game *cub)
 {
 	int	x;
 	int	y;
-	int	size;
 
 	y = -1;
-	if (WIDTH < HEIGHT)
-		size = (int)(WIDTH / RATIO);
-	else
-		size = (int)(HEIGHT / RATIO);
-	cub->minimap = mlx_new_image(cub->mlx, size, size);
-	while (++y < cub->map.x)
+	cub->minimap = mlx_new_image(cub->mlx, cub->map.x * 3, cub->map.y * 3);
+	while (++y < cub->map.x * 3)
 	{
 		x = -1;
-		while (++x < cub->map.y)
+		while (++x < cub->map.y * 3)
 		{
-			if (cub->map_matrix[x][y] == '0')
-				put_square(cub, x, y, 0xffffff88);
-			else if (cub->map_matrix[x][y] != '\n')
-				put_square(cub, x, y, 0x00000088);
+			if (cub->map_matrix[x / 3][y / 3] == '0')
+				mlx_put_pixel(cub->minimap, y, x, 0xffffff95);
+			else if (cub->map_matrix[x / 3][y / 3] != '\n')
+				mlx_put_pixel(cub->minimap, y, x, 0x00000095);
 		}
 	}
 	mlx_image_to_window(cub->mlx, cub->minimap, MAP_OFFSET, MAP_OFFSET);
@@ -41,47 +36,21 @@ void	draw_minimap(t_game *cub)
 
 void	draw_player_on_minimap(t_game *cub)
 {
-	int	i;
-	int	j;
-	int	size_x;
-	int	size_y;
+	int	x;
+	int	y;
 
-	size_x = (cub->mini_size.x / cub->map.x);
-	size_y = (cub->mini_size.y / cub->map.y);
 	if (cub->miniplayer)
 		mlx_delete_image(cub->mlx, cub->miniplayer);
-	cub->miniplayer = mlx_new_image(cub->mlx, size_x, size_y);
-	i = -1;
-	while (++i < size_x)
+	cub->miniplayer = mlx_new_image(cub->mlx, 3, 3);
+	x = -1;
+	while (++x < 3)
 	{
-		j = -1;
-		while (++j < size_y)
-			mlx_put_pixel(cub->miniplayer, j, i, 0xc1121f88);
+		y = -1;
+		while (++y < 3)
+			mlx_put_pixel(cub->miniplayer, y, x, 0xc1121f95);
 	}
 	mlx_image_to_window(cub->mlx, cub->miniplayer,
-		cub->p1.x * (cub->mini_size.x / cub->map.x) + size_x,
-		cub->p1.y * (cub->mini_size.y / cub->map.y) + size_y);
+		(cub->p1.x * 3) + MAP_OFFSET, (cub->p1.y * 3) + MAP_OFFSET);
 	cub->screen->instances->z = 1;
 	cub->miniplayer->instances->z = 3;
-}
-
-void	put_square(t_game *cub, int x, int y, int color)
-{
-	int	i;
-	int	j;
-	int	size;
-
-	if (cub->map.x > cub->map.y)
-		size = cub->minimap->width / cub->map.x;
-	else
-		size = cub->minimap->width / cub->map.y;
-	i = (x * size) - 1;
-	while (++i < (x * size) + size)
-	{
-		j = (y * size) - 1;
-		while (++j < (y * size) + size)
-			mlx_put_pixel(cub->minimap, j, i, color);
-	}
-	cub->mini_size.y = i + MAP_OFFSET - 1;
-	cub->mini_size.x = j + MAP_OFFSET - 1;
 }
