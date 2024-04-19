6e9f136ce9cf1d15af562f06e3a88a603dc50f49
Juliany <julianybernardo@outlook.com>
2024-04-18 23:31:59 -0300

    (refactor) Add NULL protection for the set string and removed '\0' check as it evaluates for every string/set combination being valid

--- libft/ft_strschr.c
+++ libft/ft_strschr.c
@@ -6,7 +6,7 @@
 /*   By: julberna <julberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 20:42:58 by julberna          #+#    #+#             */
-/*   Updated: 2024/04/18 20:46:47 by julberna         ###   ########.fr       */
+/*   Updated: 2024/04/18 21:02:25 by julberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strschr(const char *s, const char *set)
 	int	i;
 	int	j;
 
-	if (s == NULL)
+	if (s == NULL || set == NULL)
 		return (NULL);
 	i = -1;
 	while (s[++i])
@@ -28,12 +28,6 @@ char	*ft_strschr(const char *s, const char *set)
 			if (s[i] == set[j])
 				return ((char *)s + i);
 		}
-	}
-	j = -1;
-	while (set[++j])
-	{
-		if (set[j] == '\0' && s[i] == '\0')
-			return ((char *)s + i);
 	}
 	return (NULL);
 }
